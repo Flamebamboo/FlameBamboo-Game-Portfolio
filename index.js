55a2c6b01e0fc88ab0f4d4a676cc37070a3b6da6
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2024-10-02 02:08:52 +1300

    Split the main page to multiple different page
    Temporary alert prompt on load by ChatGPT since this site is public and its still in development I needed this lol

--- index.js
+++ index.js
@@ -6,4 +6,51 @@
 
 // var sum = a = b
 
+
+
+var modal = document.getElementById("myModal");
+
+window.onload = function() {
+    modal.style.display = "block";
+};
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on (x), close
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
 // document.write('The sum is: ' + sum)
+
+
+// const minusButton = document.getElementById("decrease");
+// const plusButton = document.getElementById("increase");
+// const resetButton = document.getElementById("reset");
+// const countLabel = document.getElementById("count")
+// let count = 0;
+
+
+// plusButton.onclick = function(){
+//     count++;
+//     countLabel.textContent = count
+// }
+
+
+
+// minusButton.onclick = function(){
+//     count--;
+//     countLabel.textContent = count
+// }
+
+
+// resetButton.onclick = function(){
+//     count = 0;
+//     countLabel.textContent = count
+// }
